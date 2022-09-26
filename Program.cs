6c484b722842b8133781f20794029b25f4a1b294
Polarien <Patrikrjespersen@gmail.com>
2022-09-26 08:32:27 +0200

    Good enough for confirmation
    not working

--- Program.cs
+++ Program.cs
@@ -14,9 +14,20 @@
 //Buy, Sell, Accelerate, Move
 
 // Suggestion to get started on user interaction in the console
-/*
 
-CarDealer dealer = new CarDealer();
+
+
+#endregion
+
+
+
+#region 
+
+Console.WriteLine("Hello Customer");
+Console.WriteLine("What do you want to do");
+string answer = Console.ReadLine();
+
+CarDealer cd = new CarDealer();
 while(true){
 
     Console.WriteLine("Write an action [buy, sell, exit]");
@@ -25,7 +36,9 @@ while(true){
     switch (input)
     {
         case "buy":
-            //Car myNewCar = dealer.buy(...);
+            Car firstcar = cd.availablecar[1];
+            Car myNewCar = cd.Buy(firstcar);
+            Console.WriteLine("this new car cost a lot");
             break;
         case "sell":
             break;
@@ -35,37 +48,88 @@ while(true){
             return;
     }
 }
-*/
-#endregion
 
-#region 
+class CarDealer{
+   public List<Car> availablecar =new List<Car>();
+
+    public CarDealer(){
+        Brand b = new Brand();
+        Colour c = new Colour();
+        Gear g = new Gear();
+        Tires t = new Tires();
+        Model m = new Model();
+        Price p = new Price();
+        for(int i=1; i<10; i++){
+            availablecar.Add(new Car(b, c, g, t, m, p));
+        }
+    }
+
+    public string Buy(Car car){
+        foreach(Car c in availablecar){
+            
+        }
+    }
+
+}
 class Car{
-    public string brand;
-    public string colour;
-    public string gear;
-    public string Tires;
-    public string model;
+    public Brand brand;
+    public Colour colour;
+    public Gear gear;
+    public Tires tires;
+    public Model model;
+    public Price price;
+
+    public Car(Brand brand, Colour colour, Gear gear, Tires tires, Model model, Price price){
+        this.brand =brand;
+        this.colour =colour;
+        this.gear =gear;
+        this.tires =tires;
+        this.model =model;
+        this.price =price;
+    }
+}
+class Brand{
+    public string BMW;
+    public string AUDI;
+    public string MERCEDES;
+    public string FORD;
+    public string FERRARI;
 }
 
-class brand{
-//BMW, AUDI, MERCEDES, FORD, FERRARI
-
+class Colour{
+    public string BLACK;
+    public string WHITE;
+    public string RED;
+    public string ORANGE;
+    public string GRAY;
+    public string BLUE;
+    public string GREEN;
+    public string YELLOW;
 }
 
-class colour{
-//BLACK, WHITE, RED, ORANGE, GRAY, BLUE, GREEN, YELLOW.
-}
-
-class gear{
-// MANUEL, AUTOMATIC.
+class Gear{
+    public string MANUEL;
+    public string AUTOMATIC;
 }
 
 class Tires{
-//SUMMER, WINTER, SPIKES, ALLROUND.
+    public string SUMMER;
+    public string WINTER;
+    public string SPIKES;
+    public string ALLROUND;
 
 }
 
-class model{
-//TOURING, RANGER, SEDAN, COUPE.
+class Model{
+    public string TOURING;
+    public string RANGER;
+    public string SEDAN;
+    public string COUPE;
 }
+
+class Price{
+
+}
+
+
 #endregion
